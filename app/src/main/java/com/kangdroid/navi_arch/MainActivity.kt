685ca8c1bbcbd50435bd892a65608f2c767f16f9
KangDroid <hyunwoo286@gmail.com>
2021-04-24 15:36:10 +0900

    MainActivity: Comment

--- app/src/main/java/com/kangdroid/navi_arch/MainActivity.kt
+++ app/src/main/java/com/kangdroid/navi_arch/MainActivity.kt
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.kangdroid.navi_arch.databinding.ActivityMainBinding
 
+// The View
 class MainActivity : AppCompatActivity() {
 
     private val activityMainBinding: ActivityMainBinding by lazy {
