3de90f2ff9ae1f72740c2b895071752d740f0f98
KangDroid <hyunwoo286@gmail.com>
2021-04-24 15:23:44 +0900

    MainActivity: Adapt ViewBinding

--- app/src/main/java/com/kangdroid/navi_arch/MainActivity.kt
+++ app/src/main/java/com/kangdroid/navi_arch/MainActivity.kt
@@ -2,10 +2,16 @@ package com.kangdroid.navi_arch
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.kangdroid.navi_arch.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private val activityMainBinding: ActivityMainBinding by lazy {
+        ActivityMainBinding.inflate(layoutInflater)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(activityMainBinding.root)
     }
 }
