71aaa6ecde0f63447e2b90a12e58f7298911733a
KangDroid <hyunwoo286@gmail.com>
2021-04-29 15:13:05 +0900

    [PagerViewModelTest] Initiate PagerViewModel Test

---
+++ app/src/test/java/com/kangdroid/navi_arch/viewmodel/FakeServerManagement.kt
@@ -0,0 +1,56 @@
+package com.kangdroid.navi_arch.viewmodel
+
+import com.kangdroid.navi_arch.data.FileData
+import com.kangdroid.navi_arch.data.FileType
+import com.kangdroid.navi_arch.data.dto.response.DownloadResponse
+import com.kangdroid.navi_arch.data.dto.response.RootTokenResponseDto
+import com.kangdroid.navi_arch.server.ServerInterface
+import okhttp3.MediaType.Companion.toMediaTypeOrNull
+import okhttp3.MultipartBody
+import okhttp3.ResponseBody
+
+class FakeServerManagement : ServerInterface {
+    override fun initWholeServerClient(): Boolean = true
+
+    override fun getRootToken(): RootTokenResponseDto = RootTokenResponseDto(
+        rootToken = "TestingRootToken"
+    )
+
+    override fun getInsideFiles(requestToken: String): List<FileData> = listOf(
+        FileData(
+            id = 10,
+            fileName = "/tmp/a.txt",
+            fileType = FileType.File.toString(),
+            token = "/tmp/a.txt.token",
+            lastModifiedTime = System.currentTimeMillis()
+        ),
+        FileData(
+            id = 10,
+            fileName = "/tmp/b.txt",
+            fileType = FileType.File.toString(),
+            token = "/tmp/b.txt.token",
+            lastModifiedTime = System.currentTimeMillis()
+        ),
+        FileData(
+            id = 10,
+            fileName = "/tmp/c.txt",
+            fileType = FileType.File.toString(),
+            token = "/tmp/c.txt.token",
+            lastModifiedTime = System.currentTimeMillis()
+        ),
+        FileData(
+            id = 10,
+            fileName = "/tmp/test",
+            fileType = FileType.Folder.toString(),
+            token = "/tmp/test.token",
+            lastModifiedTime = System.currentTimeMillis()
+        )
+    )
+
+    override fun upload(Param: HashMap<String, Any>, file: MultipartBody.Part): String = "10"
+
+    override fun download(token: String): DownloadResponse = DownloadResponse(
+        fileContents = ResponseBody.create("multipart/form-data".toMediaTypeOrNull(), "test"),
+        fileName = "TestingFileName"
+    )
+}
---
+++ app/src/test/java/com/kangdroid/navi_arch/viewmodel/PagerViewModelTest.kt
@@ -0,0 +1,179 @@
+package com.kangdroid.navi_arch.viewmodel
+
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.Observer
+import com.kangdroid.navi_arch.adapter.FileAdapter
+import com.kangdroid.navi_arch.data.FileData
+import com.kangdroid.navi_arch.data.FileType
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+import java.util.concurrent.CountDownLatch
+import java.util.concurrent.TimeUnit
+import java.util.concurrent.TimeoutException
+import kotlin.reflect.KFunction
+import kotlin.reflect.KProperty1
+import kotlin.reflect.full.declaredMemberFunctions
+import kotlin.reflect.full.declaredMemberProperties
+import kotlin.reflect.jvm.isAccessible
+
+class PagerViewModelTest {
+
+    // Fake Server
+    private val fakeServerManagement: FakeServerManagement = FakeServerManagement()
+
+    // Target
+    private lateinit var pagerViewModel: PagerViewModel
+
+    // Rule that every android-thread should launched in single thread
+    @get:Rule
+    var instantExecutorRule = InstantTaskExecutorRule()
+
+    /* Copyright 2019 Google LLC.
+   SPDX-License-Identifier: Apache-2.0 */
+    fun <T> LiveData<T>.getOrAwaitValue(
+        time: Long = 2,
+        timeUnit: TimeUnit = TimeUnit.SECONDS
+    ): T {
+        var data: T? = null
+        val latch = CountDownLatch(1)
+        val observer = object : Observer<T> {
+            override fun onChanged(o: T?) {
+                data = o
+                latch.countDown()
+                this@getOrAwaitValue.removeObserver(this)
+            }
+        }
+
+        this.observeForever(observer)
+
+        // Don't wait indefinitely if the LiveData is not set.
+        if (!latch.await(time, timeUnit)) {
+            throw TimeoutException("LiveData value was never set.")
+        }
+
+        @Suppress("UNCHECKED_CAST")
+        return data as T
+    }
+
+    private fun<T> getFields(fieldName: String): T {
+        // Get Page Set
+        val tmpSet: KProperty1<PagerViewModel, *> =
+            PagerViewModel::class.declaredMemberProperties.find {
+                it.name == fieldName
+            }!!.apply {
+                isAccessible = true
+            }
+
+        @Suppress("UNCHECKED_CAST")
+        return tmpSet.get(pagerViewModel) as T
+    }
+
+    private fun getFunction(functionName: String): KFunction<*> {
+        // Private Method testing
+        return PagerViewModel::class.declaredMemberFunctions.find {
+            it.name == functionName
+        }!!.apply {
+            isAccessible = true
+        }
+    }
+
+    // Fake adapter
+    private val fakeFileAdapter: FileAdapter = FileAdapter(
+        onClick = { _, _ -> },
+        onLongClick = { true },
+        fileList = listOf(
+            FileData(
+                id = 10,
+                fileName = "/tmp/a.txt",
+                fileType = FileType.File.toString(),
+                token = "/tmp/a.txt.token",
+                lastModifiedTime = System.currentTimeMillis()
+            ),
+        ),
+        pageNumber = 10,
+        currentFolder = FileData(
+            id = 1,
+            fileName = "/tmp",
+            fileType = FileType.File.toString(),
+            token = "/tmp.token",
+            lastModifiedTime = System.currentTimeMillis()
+        )
+    )
+
+    @Before
+    fun createViewModel() {
+        pagerViewModel = PagerViewModel(fakeServerManagement)
+    }
+
+    @Test
+    fun is_updatePageAndNotify_works_well_no_cache() {
+        // Private Method testing
+        getFunction("updatePageAndNotify")
+            .call(pagerViewModel, fakeFileAdapter, "test_token", false)
+
+        // Get Live Data
+        val livePagerData: MutableList<FileAdapter>? =
+            pagerViewModel.livePagerData.getOrAwaitValue()
+
+        // Live Set Test
+        assertThat(livePagerData).isNotEqualTo(null)
+        assertThat(livePagerData!!.size).isEqualTo(1)
+
+        // Get Value for set
+        val pageSet: MutableSet<String> = getFields("pageSet")
+
+        // Page Set Test
+        assertThat(pageSet.size).isEqualTo(1)
+        assertThat(pageSet.contains("test_token")).isEqualTo(true)
+
+        // Page List
+        val pageList: MutableList<FileAdapter> = getFields("pageList")
+
+        // Page List Test
+        assertThat(pageList.size).isEqualTo(1)
+        assertThat(pageList[0].currentFolder.token).isEqualTo("/tmp.token")
+        assertThat(pageList[0].fileList.size).isEqualTo(1)
+
+        // Page Cache - should be null
+        val pageCache: HashMap<String, FileAdapter> = getFields("pageCache")
+        assertThat(pageCache.size).isEqualTo(0)
+    }
+
+    @Test
+    fun is_updatePageAndNotify_works_well_with_cache() {
+        getFunction("updatePageAndNotify")
+            .call(pagerViewModel, fakeFileAdapter, "test_token", true)
+
+        // Get Live Data
+        val livePagerData: MutableList<FileAdapter>? =
+            pagerViewModel.livePagerData.getOrAwaitValue()
+
+        // Live Set Test
+        assertThat(livePagerData).isNotEqualTo(null)
+        assertThat(livePagerData!!.size).isEqualTo(1)
+
+        // Get Value for set
+        val pageSet: MutableSet<String> = getFields("pageSet")
+
+        // Page Set Test
+        assertThat(pageSet.size).isEqualTo(1)
+        assertThat(pageSet.contains("test_token")).isEqualTo(true)
+
+        // Page List
+        val pageList: MutableList<FileAdapter> = getFields("pageList")
+
+        // Page List Test
+        assertThat(pageList.size).isEqualTo(1)
+        assertThat(pageList[0].currentFolder.token).isEqualTo("/tmp.token")
+        assertThat(pageList[0].fileList.size).isEqualTo(1)
+
+        // Page Cache - should be null
+        val pageCache: HashMap<String, FileAdapter> = getFields("pageCache")
+        assertThat(pageCache.size).isEqualTo(1)
+        assertThat(pageCache["test_token"]).isNotEqualTo(null)
+        assertThat(pageCache["test_token"]!!.pageNumber).isEqualTo(fakeFileAdapter.pageNumber)
+    }
+}
