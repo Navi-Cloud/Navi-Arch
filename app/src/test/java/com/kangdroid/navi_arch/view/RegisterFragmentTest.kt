ad17ccd089399aafc28bea54f253106b716353bc
0JIEUN0 <jieun4436@naver.com>
2021-07-18 00:14:54 +0900

    RegisterFragmentTest: add viewBinding, onDestroyView() test

---
+++ app/src/test/java/com/kangdroid/navi_arch/view/RegisterFragmentTest.kt
@@ -0,0 +1,41 @@
+package com.kangdroid.navi_arch.view
+
+import android.os.Build
+import androidx.fragment.app.testing.launchFragmentInContainer
+import androidx.lifecycle.Lifecycle.State
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import com.kangdroid.navi_arch.R
+import com.kangdroid.navi_arch.databinding.FragmentRegisterBinding
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.assertj.core.api.Assertions.assertThat
+import org.robolectric.annotation.Config
+
+@Config(sdk = [Build.VERSION_CODES.P])
+@RunWith(AndroidJUnit4::class)
+class RegisterFragmentTest {
+    @Test
+    fun is_viewBinding_ok() {
+        val scenario = launchFragmentInContainer<RegisterFragment>(
+            themeResId = R.style.Theme_NaviArch // If you don't do this, this will throw error while inflating material view
+        )
+        scenario.moveToState(State.CREATED)
+        scenario.onFragment{
+            assertThat(it.registerBinding is FragmentRegisterBinding?).isEqualTo(true)
+            assertThat(it).isNotEqualTo(null)
+        }
+    }
+
+    @Test
+    fun is_onDestroyView_works_well(){
+        val scenario = launchFragmentInContainer<RegisterFragment>(
+            themeResId = R.style.Theme_NaviArch
+        )
+        scenario.onFragment {
+            it.onDestroyView()
+            assertThat(it.registerBinding is FragmentRegisterBinding?).isEqualTo(true)
+            assertThat(it.registerBinding).isEqualTo(null)
+        }
+        scenario.moveToState(State.DESTROYED)
+    }
+}
