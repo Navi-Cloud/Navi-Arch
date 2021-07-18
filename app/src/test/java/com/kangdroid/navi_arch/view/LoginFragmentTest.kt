0cca2d090b342f5ad674e526d1629fa3f3028fed
jiwoo <hjw6112@gmail.com>
2021-07-18 18:04:46 +0900

    viewBinding, destroyView Test

--- app/src/test/java/com/kangdroid/navi_arch/view/LoginFragmentTest.kt
+++ app/src/test/java/com/kangdroid/navi_arch/view/LoginFragmentTest.kt
@@ -1,23 +1,41 @@
 package com.kangdroid.navi_arch.view
 
+//import androidx.test.espresso.Espresso.onView
+//import androidx.test.espresso.matcher.ViewMatchers.withText
+//import androidx.test.espresso.matcher.ViewMatchers.withId
+//import androidx.test.espresso.action.ViewActions.click
+//
+//import androidx.test.espresso.assertion.ViewAssertions.matches
+
 import android.os.Build
-import androidx.fragment.app.testing.launchFragment
+import android.os.IBinder
+import android.provider.DocumentsContract
+import android.view.WindowManager
+import android.widget.Button
+import android.widget.TextView
 import androidx.fragment.app.testing.launchFragmentInContainer
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.Lifecycle.State
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import com.kangdroid.navi_arch.R
-import com.kangdroid.navi_arch.databinding.FragmentRegisterBinding
-import org.junit.runner.RunWith
-import org.assertj.core.api.Assertions.assertThat
-import com.kangdroid.navi_arch.viewmodel.UserViewModel
-import org.junit.*
-import org.robolectric.annotation.Config
-import androidx.lifecycle.Lifecycle.State
 import com.kangdroid.navi_arch.databinding.FragmentLoginBinding
+import com.kangdroid.navi_arch.viewmodel.PageRequest
+import com.kangdroid.navi_arch.viewmodel.UserViewModel
+import com.kangdroid.navi_arch.viewmodel.ViewModelTestHelper.getOrAwaitValue
+import junit.framework.Assert.assertEquals
+import org.assertj.core.api.Assertions.assertThat
+import org.hamcrest.Description
+import org.hamcrest.TypeSafeMatcher
+import org.junit.*
+import org.junit.runner.RunWith
+import org.mockito.Mock
+import org.mockito.Mockito
+import org.robolectric.annotation.Config
+
 
 @Config(sdk = [Build.VERSION_CODES.P])
 @RunWith(AndroidJUnit4::class)
 class LoginFragmentTest {
-
     private lateinit var userViewModel: UserViewModel
 
     @Before
@@ -49,6 +67,72 @@ class LoginFragmentTest {
             assertThat(it.loginBinding).isEqualTo(null)
         }
         scenario.moveToState(State.DESTROYED)
+    }
+
+    //회원가입 버튼 누름
+    @Test
+    fun change_registerView(){
+        val scenario = launchFragmentInContainer<LoginFragment>(
+            themeResId = R.style.Theme_NaviArch
+        ).apply {
+            moveToState(Lifecycle.State.STARTED)
+        }
+        scenario.onFragment {
+            val button = it.view?.findViewById<TextView>(R.id.textView2)
+            button?.performClick().also { clickResult ->
+                assertThat(clickResult).isEqualTo(true)
+                assertThat(userViewModel.pageRequest.value).isEqualTo(PageRequest.REQUEST_REGISTER)
+            }
+        }
+    }
+
+    //로그인 버튼 눌렀을 때 에러
+    @Test
+    fun login_is_fail(){
+        val scenario = launchFragmentInContainer<LoginFragment>(
+            themeResId = R.style.Theme_NaviArch
+        ).apply {
+            moveToState(Lifecycle.State.STARTED)
+        }
+
+        scenario.onFragment {
+            it.loginBinding!!.idLogin.setText("userid")
+            it.loginBinding!!.pwLogin.setText("password")
+
+            val exception : Throwable = Throwable()
+            userViewModel.loginError(exception)
+
+            it.loginBinding!!.button.performClick().also { clickResult ->
+                assertThat(clickResult).isEqualTo(true)
+            }
+
+            assertThat(userViewModel.loginErrorData.value).isNotEqualTo(null)
+            assertThat(it.loginBinding!!.idLogin.text.toString()).isEqualTo("")
+            assertThat(it.loginBinding!!.pwLogin.text.toString()).isEqualTo("")
+
+        }
+    }
+
+    //로그인 버튼 눌렀을 때 성공
+    @Test
+    fun login_is_well(){
+        val scenario = launchFragmentInContainer<LoginFragment>(
+            themeResId = R.style.Theme_NaviArch
+        ).apply {
+            moveToState(Lifecycle.State.STARTED)
+        }
+
+        scenario.onFragment {
+            it.loginBinding!!.idLogin.setText("userid")
+            it.loginBinding!!.pwLogin.setText("password")
+
+            it.loginBinding!!.button.performClick().also { clickResult ->
+                assertThat(clickResult).isEqualTo(true)
+            }
+
+            assertThat(it.loginBinding!!.idLogin.text.toString()).isEqualTo("userid")
+            assertThat(it.loginBinding!!.pwLogin.text.toString()).isEqualTo("password")
+        }
     }
 
 }
