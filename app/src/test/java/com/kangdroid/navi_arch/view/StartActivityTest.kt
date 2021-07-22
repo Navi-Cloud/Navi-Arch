f8d6d6468dcbe47e6847f9949316da52a97fe2c8
mimwin <kms105903@naver.com>
2021-07-22 21:53:39 +0900

    StartActivityTest.kt : Test FragmentTransaction, UserViewModel LiveData

--- app/src/test/java/com/kangdroid/navi_arch/view/StartActivityTest.kt
+++ app/src/test/java/com/kangdroid/navi_arch/view/StartActivityTest.kt
@@ -1,105 +1,137 @@
 package com.kangdroid.navi_arch.view
 
-import android.app.Application
-import android.content.Context
 import android.content.Intent
 import android.os.Build
+import android.view.View
+import android.widget.TextView
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentTransaction
 import androidx.fragment.app.testing.launchFragmentInContainer
 import androidx.lifecycle.Lifecycle
-import androidx.lifecycle.ViewModel
 import androidx.test.core.app.ActivityScenario
-import androidx.test.core.app.ApplicationProvider
-import androidx.test.ext.junit.rules.activityScenarioRule
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import com.kangdroid.navi_arch.R
-import com.kangdroid.navi_arch.databinding.ActivityStartBinding
 import com.kangdroid.navi_arch.viewmodel.PageRequest
 import com.kangdroid.navi_arch.viewmodel.UserViewModel
-import com.kangdroid.navi_arch.viewmodel.ViewModelTestHelper
-import com.kangdroid.navi_arch.viewmodel.ViewModelTestHelper.getFunction
 import com.kangdroid.navi_arch.viewmodel.ViewModelTestHelper.getOrAwaitValue
-import junit.framework.TestCase
-import org.assertj.core.api.Assertions
-import org.assertj.core.api.Java6Assertions.assertThat
-import org.junit.Before
+import junit.framework.Assert.assertEquals
+import junit.framework.Assert.assertNotNull
+import org.assertj.core.api.Assertions.assertThat
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.robolectric.Robolectric
-import org.robolectric.RuntimeEnvironment
 import org.robolectric.Shadows.shadowOf
 import org.robolectric.annotation.Config
-import org.robolectric.util.FragmentTestUtil
 import org.robolectric.util.FragmentTestUtil.startFragment
-import kotlin.reflect.KFunction
+import kotlin.reflect.full.declaredMembers
+import kotlin.reflect.jvm.isAccessible
 
 
 @Config(sdk = [Build.VERSION_CODES.P])
 @RunWith(AndroidJUnit4::class)
-class StartActivityTest : TestCase(){
+class StartActivityTest {
 
-    private var context: Context? = null
     private lateinit var scenario : ActivityScenario<StartActivity>
-    private var userViewModel : UserViewModel = UserViewModel()
-    private val activity = Robolectric.setupActivity(StartActivity::class.java)
+    private val activity : StartActivity = Robolectric.setupActivity(StartActivity::class.java)
 
-    @Before
-    fun init(){
-        context = ApplicationProvider.getApplicationContext()
+    @get:Rule
+    val taskExecutorRule = InstantTaskExecutorRule()
+
+    private inline fun <reified T> getUserViewModel(receiver: T): UserViewModel {
+        val memberProperty = T::class.declaredMembers.find { it.name == "userViewModel" }!!
+        memberProperty.isAccessible = true
+        return memberProperty.call(receiver) as UserViewModel
     }
-
 
     @Test
     fun is_ViewBinding_well() {
         scenario = ActivityScenario.launch(StartActivity::class.java).moveToState(Lifecycle.State.CREATED)
         scenario.onActivity {
-            assert(it.startBinding is ActivityStartBinding).equals(true)
+            assertNotNull(it.startBinding)
         }
         scenario.close()
     }
 
-//    @Test
-//    fun is_initObserver_MAIN_well(){
-//        ViewModelTestHelper.getFunction<UserViewModel>("requestMainPage")
-//            .call(userViewModel)
-//
-//        val intent = Intent(activity,MainActivity::class.java)
-//        userViewModel.pageRequest.getOrAwaitValue().also{
-//            Assertions.assertThat(it).isEqualTo(PageRequest.REQUEST_MAIN)
-//            val actual : Intent = shadowOf(activity).nextStartedActivity
-//            print(intent.component)
-//            print("SEcond"+actual.component)
-//            assertEquals(intent,actual.component)
-//        }
-//    }
+    @Test
+    fun is_initObserver_MAIN_well(){
+
+        val one = Robolectric.buildActivity(StartActivity::class.java)
+            .create()
+            .resume()
+            .get()
+
+        scenario = ActivityScenario.launch(StartActivity::class.java).moveToState(Lifecycle.State.CREATED)
+        scenario.onActivity {
+
+            val userViewModel = getUserViewModel(it)
+            userViewModel.requestMainPage()
+
+            runCatching {
+                userViewModel.pageRequest.getOrAwaitValue()
+            }.onSuccess {
+                assertThat(it).isEqualTo(PageRequest.REQUEST_MAIN)
+            }.onFailure { throwable ->
+                println(throwable.stackTraceToString())
+            }
+
+            val actual = Intent(one,MainActivity::class.java)
+            val shadowactivity = shadowOf(one)
+            val intent = shadowOf(shadowactivity.nextStartedActivity)
+            assertEquals(intent.intentClass.name,MainActivity::class.java.name)
+        }
+    }
+
 
     @Test
     fun is_initObserver_LOGIN_well(){
-        ViewModelTestHelper.getFunction<UserViewModel>("requestLoginPage")
-            .call(userViewModel)
+        scenario = ActivityScenario.launch(StartActivity::class.java).moveToState(Lifecycle.State.CREATED)
+        scenario.onActivity {
 
-        userViewModel.pageRequest.getOrAwaitValue().also{
-            Assertions.assertThat(it).isEqualTo(PageRequest.REQUEST_LOGIN)
-            val fragmentScenario = launchFragmentInContainer<LoginFragment>(
-                themeResId = R.style.Theme_NaviArch // If you don't do this, this will throw error while inflating material view
-            )
-            assertNotNull(fragmentScenario)
+            val userViewModel = getUserViewModel(it)
+
+            runCatching {
+                userViewModel.pageRequest.getOrAwaitValue()
+            }.onSuccess {
+                assertThat(it).isEqualTo(PageRequest.REQUEST_LOGIN)
+            }.onFailure {
+                println(it.stackTraceToString())
+            }
+
+//            val fragmentManager = activity.supportFragmentManager
+//            val trans : FragmentTransaction = fragmentManager.beginTransaction()
+//            trans.replace(R.id.startActivityContainer,loginFragment)
+
+            assertNotNull(activity.transaction)
+
         }
     }
 
     @Test
     fun is_initObserver_REGISTER_well(){
-        ViewModelTestHelper.getFunction<UserViewModel>("requestRegisterPage")
-            .call(userViewModel)
+        scenario = ActivityScenario.launch(StartActivity::class.java).moveToState(Lifecycle.State.CREATED)
+        scenario.onActivity {
 
-        userViewModel.pageRequest.getOrAwaitValue().also{
-            Assertions.assertThat(it).isEqualTo(PageRequest.REQUEST_REGISTER)
-            val fragmentScenario = launchFragmentInContainer<RegisterFragment>(
-                themeResId = R.style.Theme_NaviArch // If you don't do this, this will throw error while inflating material view
-            )
-            assertNotNull(fragmentScenario)
+            val userViewModel = getUserViewModel(it)
+            userViewModel.requestRegisterPage()
+
+            runCatching {
+                userViewModel.pageRequest.getOrAwaitValue()
+            }.onSuccess {
+                assertThat(it).isEqualTo(PageRequest.REQUEST_REGISTER)
+            }.onFailure { throwable ->
+                println(throwable.stackTraceToString())
+            }
+
+            assertNotNull(activity.transaction)
+
+//            val fragmentScenario = launchFragmentInContainer<RegisterFragment>(
+//                themeResId = R.style.Theme_NaviArch // If you don't do this, this will throw error while inflating material view
+//            )
+//            assertNotNull(fragmentScenario)
         }
+
     }
 
 }
