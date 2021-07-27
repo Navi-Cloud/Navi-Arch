d6fd33e43020a290428f2d68798a92e075e16c85
0JIEUN0 <jieun4436@naver.com>
2021-07-27 16:15:10 +0900

    SerarchViewModel: Search file, sort, and handle error

--- app/src/main/java/com/kangdroid/navi_arch/view/SearchActivity.kt
+++ app/src/main/java/com/kangdroid/navi_arch/view/SearchActivity.kt
@@ -2,19 +2,94 @@ package com.kangdroid.navi_arch.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.kangdroid.navi_arch.R
+import android.util.Log
+import android.view.KeyEvent
+import android.view.View
+import android.widget.Toast
+import androidx.activity.viewModels
+import com.kangdroid.navi_arch.data.FileSortingMode
 import com.kangdroid.navi_arch.databinding.ActivitySearchBinding
+import com.kangdroid.navi_arch.viewmodel.SearchViewModel
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class SearchActivity : AppCompatActivity() {
+    // Log Tag
+    private val logTag: String = this::class.java.simpleName
+
     // View binding
     private val searchBinding: ActivitySearchBinding by lazy {
         ActivitySearchBinding.inflate(layoutInflater)
     }
 
+    // Search ViewModel
+    private val searchViewModel: SearchViewModel by viewModels()
+
+    // For Sorting
+    private var currentSortMode: FileSortingMode = FileSortingMode.TypedName
+    private var isReversed: Boolean = false
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(searchBinding.root)
+
+        initBinding()
+
+        initObserver()
+    }
+
+    private fun initBinding() {
+        searchBinding.apply {
+            inputSearch.setOnKeyListener { _, keyCode, _ ->
+                if (keyCode == KeyEvent.KEYCODE_ENTER){
+                    // Show ProgressBar
+                    progressBar.visibility = View.VISIBLE
+
+                    // Update sort mode
+                    updateSortMode()
+
+                    // Search file
+                    val query: String = inputSearch.text.toString()
+                    searchViewModel.search(
+                        query = query,
+                        mode = currentSortMode,
+                        isReversed = isReversed
+                    )
+                    true
+                } else false
+            }
+        }
+    }
+
+    private fun initObserver() {
+        searchViewModel.searchResultLiveData.observe(this) {
+            // Hide ProgressBar first
+            searchBinding.progressBar.visibility = View.GONE
+
+            // Handle search result
+            if (it != null) {
+
+            }
+        }
+        searchViewModel.liveErrorData.observe(this) {
+            // Hide ProgressBar first
+            searchBinding.progressBar.visibility = View.GONE
+
+            // Handle search error
+            if (it != null) {
+                Log.e(logTag, "Error Message Observed")
+                Log.e(logTag, it.stackTraceToString())
+                Toast.makeText(this, "Search Error: ${it.message}", Toast.LENGTH_LONG)
+                    .show()
+            }
+        }
+    }
+
+    private fun updateSortMode() {
+        currentSortMode = when (searchBinding.sortByNameOrLMT.isChecked) {
+            true -> if (searchBinding.sortByType.isChecked) FileSortingMode.LMT else FileSortingMode.TypedLMT
+            false -> if (searchBinding.sortByType.isChecked) FileSortingMode.Name else FileSortingMode.TypedName
+        }
+        isReversed = searchBinding.sortByAscendingOrDescending.isChecked
     }
 }
---
+++ app/src/main/java/com/kangdroid/navi_arch/viewmodel/SearchViewModel.kt
@@ -0,0 +1,71 @@
+package com.kangdroid.navi_arch.viewmodel
+
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.kangdroid.navi_arch.data.FileData
+import com.kangdroid.navi_arch.data.FileSortingMode
+import com.kangdroid.navi_arch.server.ServerInterface
+import com.kangdroid.navi_arch.server.ServerManagement
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+import javax.inject.Inject
+
+@HiltViewModel
+class SearchViewModel @Inject constructor() : ViewModel() {
+
+    // Search Result and Live data
+    var searchResultList: MutableList<FileData> = mutableListOf()
+    val searchResultLiveData: MutableLiveData<MutableList<FileData>> = MutableLiveData()
+
+    // Error Data in case of server connection issues
+    val liveErrorData: MutableLiveData<Throwable> = MutableLiveData()
+
+    // Server Management
+    private val serverManagement: ServerInterface = ServerManagement.getServerManagement()
+
+    // Get Search Result
+    fun search(query: String,
+                       mode: FileSortingMode = FileSortingMode.TypedName,
+                       isReversed: Boolean = false) {
+
+        var searchThrow : Throwable?= null
+
+        viewModelScope.launch {
+            withContext(Dispatchers.IO) {
+                runCatching {
+                    serverManagement.searchFile(
+                        searchParam = query
+                    )
+                }.onFailure {
+                    searchThrow = it
+                }.onSuccess {
+                    searchResultList = it as MutableList<FileData>
+                }
+            }
+
+            // Check Error
+            if (searchThrow != null) {
+                liveErrorData.value = searchThrow
+                return@launch
+            }
+
+            sort(mode, isReversed)
+        }
+    }
+
+    // Sort
+    fun sort(mode: FileSortingMode, isReversed: Boolean) {
+        // Sort with Comparator
+        if (isReversed) {
+            searchResultList.sortedWith(mode).asReversed()
+        } else {
+            searchResultList.sortedWith(mode)
+        }
+
+        // Notify Live Data
+        searchResultLiveData.value = searchResultList
+    }
+}
