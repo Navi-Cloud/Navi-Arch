2547e022504b3ad215356414eafa00b0020a645e
KangDroid <hyunwoo286@gmail.com>
2021-08-30 20:53:09 +0900

    Initial Commit

---
+++ app/src/main/java/com/navi/file/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.navi.file
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
