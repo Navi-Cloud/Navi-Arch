29b8edf420a83bad1ed1a0bc26281655ca6ff0b1
mimwin <kms105903@naver.com>
2021-09-23 14:19:18 +0900

    MainActivity.kt : add setOnClickListener

--- app/src/main/java/com/kangdroid/navi_arch/view/MainActivity.kt
+++ app/src/main/java/com/kangdroid/navi_arch/view/MainActivity.kt
@@ -1,16 +1,38 @@
 package com.kangdroid.navi_arch.view
 
+import android.content.Intent
+import android.os.Bundle
 import android.view.*
+import androidx.appcompat.app.AppCompatActivity
 import com.kangdroid.navi_arch.R
+import com.kangdroid.navi_arch.databinding.ActivityMainBinding
 import dagger.hilt.android.AndroidEntryPoint
 
 // The View
 @AndroidEntryPoint
-class MainActivity : PagerActivity() {
-    // Menu for dynamically hide - show
-    private var dynamicMenu: Menu? = null
+class MainActivity : AppCompatActivity() {
 
-    override val errorObserverCallback: ((Throwable) -> Unit) = {
-        dynamicMenu?.findItem(R.id.action_add_folder)?.isVisible = false
+    protected val activityMainBinding: ActivityMainBinding by lazy {
+        ActivityMainBinding.inflate(layoutInflater)
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(activityMainBinding.root)
+
+        activityMainBinding.apply {
+            mainOption.setOnClickListener {
+                val optionBottomSheetFragment = OptionBottomSheetFragment()
+                optionBottomSheetFragment.show(supportFragmentManager, optionBottomSheetFragment.tag)
+            }
+            mainSearchBar.setOnClickListener{
+                val intent = Intent(applicationContext,SearchActivity::class.java)
+                startActivity(intent)
+            }
+            mainMyfile.setOnClickListener {
+                val intent = Intent(applicationContext,MyfileActivity::class.java)
+                startActivity(intent)
+            }
+        }
     }
 }
